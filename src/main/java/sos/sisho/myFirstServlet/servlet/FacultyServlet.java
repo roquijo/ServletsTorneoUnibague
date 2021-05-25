5e8136c40e5d6670a047a6da9bd0b6e635fd05af
Juanes Gomez <juanesgor@gmail.com>
2021-05-25 15:23:37 -0500

    MG

--- src/main/java/sos/sisho/myFirstServlet/servlet/FacultyServlet.java
+++ src/main/java/sos/sisho/myFirstServlet/servlet/FacultyServlet.java
@@ -1,6 +1,8 @@
 package sos.sisho.myFirstServlet.servlet;
 
 
+import sos.sisho.myFirstServlet.user.service.UserService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,11 +13,12 @@ import java.io.IOException;
 @WebServlet(urlPatterns =  "/faculty.do")
 public class FacultyServlet extends HttpServlet {
 
-  
+
+    private  final UserService userService = new UserService();
 
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("WEB-INF/views/faculty.jsp").forward(request, response);
     }
 
