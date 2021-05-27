9f059b29cdeb6685853de2c7b062f00266f13f34
Juan Pablo Menses Bocanegra <juanweue@gmail.com>
2021-05-27 12:19:20 -0500

    Mn

--- src/main/java/sos/sisho/myFirstServlet/servlet/servletsForFaculty/DeleteFacultyServlet.java
+++ src/main/java/sos/sisho/myFirstServlet/servlet/servletsForFaculty/DeleteFacultyServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+//Mene
 
 @WebServlet(urlPatterns = "/delete-faculty.do")
 public class DeleteFacultyServlet extends HttpServlet {
